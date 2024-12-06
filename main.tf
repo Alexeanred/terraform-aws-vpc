bd1cf81de3d382eb7036c8c667c147e78d2770ff
Alexandered <duytien46kd@gmail.com>
2024-12-06 18:46:31 +0700

    fix error AZ

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnet" {
 
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.public_subnet_cidr[count.index]
-  availability_zone = var.public_subnet_cidr[count.index % length(var.public_subnet_cidr)]
+  availability_zone = var.availability_zone[count.index % length(var.public_subnet_cidr)]
 
   tags = {
     "Name" = "public-subnet"
